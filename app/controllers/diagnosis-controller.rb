34097a4014c996b709ff895d1dcdf4546786b676
Susan Tussing <susan@fionavar.net>
2016-07-13 08:07:42 -0500

    Save user's name to session & display back to them

--- app/controllers/diagnosis-controller.rb
+++ app/controllers/diagnosis-controller.rb
@@ -1,3 +1,13 @@
 MyApp.get "/" do
   erb :"diagnosis/home"
 end
+
+MyApp.post "/diagnosis/" do
+  session['name'] = params[:name]
+  redirect "/diagnosis/quiz"
+end
+
+MyApp.get "/diagnosis/quiz" do
+  @name = session['name']
+  erb :"diagnosis/quiz"
+end
