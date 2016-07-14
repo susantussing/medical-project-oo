847ab4b719b0122c64575f2831e723deb857a616
Colin Narans <cnarans@gmail.com>
2016-07-14 10:50:42 -0500

    expanded record test

--- tests/unit/record_test.rb
+++ tests/unit/record_test.rb
@@ -18,10 +18,14 @@ class RecordTest < Minitest::Test
     assert_equal(Record.getQuestion([nil,nil,nil,nil,true,nil]), "Rash;")
     assert_equal(Record.getQuestion([nil,nil,nil,nil,nil,true]), "Rash on face")
     assert_equal(Record.getQuestion([true,true,true,true,true,true]), "Cough;Productive Cough;Headache;Headache on one side;Rash;Rash on face")
+    assert_equal(Record.getQuestion([false,false,false,false,false,false]),"")
+    assert_equal(Record.getQuestion([false,nil,false,nil,false,nil]),"")
+    assert_equal(Record.getQuestion([false,false,true,true,nil,nil]),"Headache;Headache on one side;")
   end
 
   def test_readwritedelete
   	Record.saveRecord("TestName",[true,true,true,true,true,true],"Disease")
+  	Record.saveRecord("TestName",[true,true,nil,nil,nil,nil],"Disease 2")
   	result = Record.getRecords("TestName")
   	assert_equal(result[0][1][0], "Cough")
   	assert_equal(result[0][1][1], "Productive Cough")
@@ -29,6 +33,14 @@ class RecordTest < Minitest::Test
   	assert_equal(result[0][1][3], "Headache on one side")
   	assert_equal(result[0][1][4], "Rash")
   	assert_equal(result[0][1][5], "Rash on face")
+  	assert_equal(result[0][2], "Disease\n")
+  	assert_equal(result[1][1][0], "Cough")
+  	assert_equal(result[1][1][1], "Productive Cough")
+  	assert_equal(result[1][1][2], nil)
+  	assert_equal(result[1][1][3], nil)
+  	assert_equal(result[1][1][4], nil)
+  	assert_equal(result[1][1][5], nil)
+  	assert_equal(result[1][2], "Disease 2\n")
   	Record.deleteRecords("TestName")
   	result = Record.getRecords("TestName")
   	empty = []
