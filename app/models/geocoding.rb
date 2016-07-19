c03ba47234ede8c511837bb8bd239554cd5bbda2
Colin Narans <cnarans@gmail.com>
2016-07-19 11:44:27 -0500

    form styling

--- app/models/geocoding.rb
+++ app/models/geocoding.rb
@@ -18,7 +18,7 @@ class Geo
 		info = HTTParty.get("https://maps.googleapis.com/maps/api/geocode/json?address=#{query}&key=#{API_KEY}")
 		lat = info["results"][0]["geometry"]["location"]["lat"]
 		lng = info["results"][0]["geometry"]["location"]["lng"]
-		return lat.to_s+lng.to_s
+		return lat.to_s+","+lng.to_s
 	end
 
 end
