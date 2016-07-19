6ceb5c469b848c525afe0b2765f057b0e48569d8
Susan Tussing <susan@fionavar.net>
2016-07-19 13:07:32 -0500

    Fixing indentation

--- app/controllers/places-controller.rb
+++ app/controllers/places-controller.rb
@@ -1,11 +1,11 @@
 MyApp.post "/places" do
 	@street = params[:street]
-    @city = params[:city]
-    @state = params[:state]
-    @zip = params[:zip]
-    @location = Geo.getLocation(@street,@city,@state,@zip)
-  	@location = @location.split(",")
-  	@places = Places.get_clinics(@location[0],@location[1])[0,5]
-    erb :"places/list"
+  @city = params[:city]
+  @state = params[:state]
+  @zip = params[:zip]
+  @location = Geo.getLocation(@street,@city,@state,@zip)
+	@location = @location.split(",")
+	@places = Places.get_clinics(@location[0],@location[1])[0,5]
+  erb :"places/list"
 end
 
