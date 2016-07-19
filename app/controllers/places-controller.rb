dcfc3f5049ed2730d87929e5e49917103ffe75e6
Colin Narans <cnarans@gmail.com>
2016-07-19 13:06:18 -0500

    basically working

--- app/controllers/places-controller.rb
+++ app/controllers/places-controller.rb
@@ -1,4 +1,11 @@
-MyApp.get "/places" do
-  @places = Places.get_clinics(41.2461226,-95.9336376)[0,5]
-  erb :"places/list"
+MyApp.post "/places" do
+	@street = params[:street]
+    @city = params[:city]
+    @state = params[:state]
+    @zip = params[:zip]
+    @location = Geo.getLocation(@street,@city,@state,@zip)
+  	@location = @location.split(",")
+  	@places = Places.get_clinics(@location[0],@location[1])[0,5]
+    erb :"places/list"
 end
+
