682ffbb6fb3f55a5dfe63bca14405cb71bf535b4
Susan Tussing <susan@fionavar.net>
2016-07-19 11:16:42 -0500

    Add details for place search

--- app/controllers/places-controller.rb
+++ app/controllers/places-controller.rb
@@ -1,4 +1,7 @@
 MyApp.get "/places" do
-  @places = Places.get_clinics(41.2461226,-95.9336376)["results"]
+  @places = Places.get_clinics(41.2461226,-95.9336376)[0,5]
+  @places.map! do |place| 
+    Places.get_detail(place["place_id"])
+  end
   erb :"places/list"
 end
--- app/models/places.rb
+++ app/models/places.rb
@@ -1,8 +1,14 @@
 require_relative "secret.rb"
 
 class Places
+
   def Places.get_clinics (latitude, longitude)
     response = HTTParty.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=#{API_KEY}&location=#{latitude},#{longitude}&type=hospital&rankby=distance")
-    return response
+    return response["results"]
+  end
+
+  def Places.get_detail (placeID)
+    response = HTTParty.get("https://maps.googleapis.com/maps/api/place/details/json?key=#{API_KEY}&placeid=#{placeID}")
+    return response["result"]
   end
 end
