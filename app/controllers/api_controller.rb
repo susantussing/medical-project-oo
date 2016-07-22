fa21012d4f68f49241d9ea6113a5ff42bc9e59e7
Susan Tussing <susan@fionavar.net>
2016-07-22 10:47:28 -0500

    Fix JSON return content types

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,7 +1,18 @@
+require 'json'
+
 MyApp.get "/api/diseases" do
-  return Record.listDiseasesJSON
+  content_type :json
+  Record.listDiseasesJSON
 end
 
 MyApp.get "/api/diseases/:id" do
-  return Record.listSymptomsJSON(params[:id].to_i)
+  content_type :json
+  Record.listSymptomsJSON(params[:id].to_i)
+end 
+
+MyApp.get "/api/patient" do
+  content_type :json
+  name = params[:name]
+  info = Record.apiReturn(name)
+  info.to_json
 end
