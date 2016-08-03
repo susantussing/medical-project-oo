e23c9f2f859efd21c077317af5fb754d14075192
Susan Tussing <susan@fionavar.net>
2016-08-03 14:54:27 -0500

    Set up disease and symptom tables

--- app/models/disease.rb
+++ app/models/disease.rb
@@ -3,6 +3,16 @@
 # id INTEGER PRIMARY KEY,
 # name TEXT
 # );
+#
+#
+# INSERT INTO diseases (name)
+# VALUES
+# ("lung cancer"),
+# ("pneumonia"),
+# ("brain cancer"),
+# ("migraine"),
+# ("skin cancer"),
+# ("lupus");
 class Disease
 
 
--- app/models/symptom.rb
+++ app/models/symptom.rb
@@ -1,7 +1,8 @@
 # CREATE TABLE symptoms
 # (
 # id INTEGER PRIMARY KEY,
-# name TEXT
+# name TEXT,
+# question TEXT
 # );
 
 # CREATE TABLE disease_symptoms
@@ -10,6 +11,30 @@
 #   disease_id INTEGER,
 #   symptom_id INTEGER
 # );
+#
+# INSERT INTO symptoms (name, question)
+# VALUES
+# ("cough","Do you have a cough?"),
+# ("headache","Do you have a headache?"),
+# ("rash","Do you have a rash?"),
+# ("fatigue","Are you feeling tired or run down?"),
+# ("fever", "Are you running a fever?"),
+# ("growth", "Do you have any abnormal growths or moles on your skin?"),
+# ("aura", "Do you have visual disturbances or see an aura around objects?");
+
+# INSERT INTO disease_symptoms (disease_id, symptom_id)
+# VALUES
+# (1, 1),
+# (1, 4),
+# (2, 1),
+# (2, 5),
+# (3, 2),
+# (3, 4),
+# (4, 2),
+# (4, 7),
+# (5, 6),
+# (6, 3),
+# (6, 4);
 
 class Symptoms
 
